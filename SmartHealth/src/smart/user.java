93fce0cc358cea126e9008adc91089cba7a18dd4
Harsh1-1 <harsh16023@iiitd.ac.in>
2016-08-22 22:46:27 +0530

    just a test commit

---
+++ SmartHealth/src/smart/user.java
@@ -0,0 +1,17 @@
+package smart;
+
+public class user {
+
+	private String Username;
+	private String Primaryemail;
+	private String Secondaryemail;
+	private String Password;
+	private String Firstname;
+	private String Lastname;
+	private String Postaladdress;
+	private String Aboutme;
+	private String picurl[] = new String[3];
+	private boolean has_quit;
+	
+	
+}
