312fd36fb8a32e6176a2b7d5bc1e86a31e486566
Harsh1-1 <harsh16023@iiitd.ac.in>
2016-08-23 23:42:44 +0530

    added qualifications

--- SmartHealth/src/smart/Moderator.java
+++ SmartHealth/src/smart/Moderator.java
@@ -9,11 +9,12 @@ public class Moderator extends User{
 	public	Moderator(String FirstName, String LastName,String PrimaryEmail,
 			String SecondaryEmail, String Password, String UserId,
 			String PostalAddress, String AboutMe, String picurl1,
-			String picurl2, String picurl3, String EmergencyContact)
+			String picurl2, String picurl3, String EmergencyContact, ArrayList<String> Qualifications)
 	{
 		super(FirstName, LastName, PrimaryEmail, SecondaryEmail, Password, UserId, PostalAddress, AboutMe, picurl1, picurl2, picurl3);
 		this.EmergencyContact = EmergencyContact;
 		this.UserType="MOD";
+		this.Qualifications = Qualifications;
 	}
 	public void setEmergencyContact(String EmergencyContact)
 	{
