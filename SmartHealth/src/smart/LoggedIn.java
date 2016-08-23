5b2013ed1d6b3cabc9bff6b4bd52c9dfba8df066
Abhiprayah <abhiprayahtiwari@gmail.com>
2016-08-23 14:25:40 +0530

    Bugs and changes modified

--- SmartHealth/src/smart/LoggedIn.java
+++ SmartHealth/src/smart/LoggedIn.java
@@ -14,11 +14,21 @@ class LoggedIn extends State{
 	State handle(){
 		System.out.println("1. Update profile info");
 		System.out.println("2. Quit Profile");
+		System.out.println("3. Logout");
 		System.out.println("Enter your choice ");
 		int choice = sc.nextInt();
 		switch(choice){
-		case 1 : return new Update(sc);
-		case 2 : break;
+		case 1 : 
+			return new Update(sc);
+		case 2 : 
+			SmartHealth.curUser.quit();
+			SmartHealth.curUser = null;
+			SmartHealth.curPos = -1;
+			return new Login(sc);
+		case 3 : 
+			SmartHealth.curUser = null;
+			SmartHealth.curPos = -1;
+			return new Login(sc);
 		default : System.out.println("Invalid choice. Please enter a valid choice.");
 		}
 		return this;
