37b0121e22c65339e024e9c31191089524a5b891
Harsh1-1 <harsh16023@iiitd.ac.in>
2016-09-10 18:16:26 +0530

    added sql queries

---
+++ sqlqueriesforqualandtype.sql
@@ -0,0 +1,13 @@
+
+
+insert into smarthealthdb.qualification values(1,"M.D"); 
+insert into smarthealthdb.qualification values(2,"MBBS"); 
+insert into smarthealthdb.qualification values(3,"Dr.MED"); 
+insert into smarthealthdb.qualification values(4,"BMBS"); 
+
+
+insert into smarthealthdb.usertype values(1,"ADMIN");
+insert into smarthealthdb.usertype values(2,"MOD");
+insert into smarthealthdb.usertype values(3,"NEW");
+insert into smarthealthdb.usertype values(4,"MIDDLE");
+insert into smarthealthdb.usertype values(5,"OLD");
